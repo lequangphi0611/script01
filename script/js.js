e9f43e290f299a5906321a0561f3cf6e2515494a
lequangphi0611 <lequangphi0611@gmail.com>
2018-10-13 20:08:15 +0700

    origin

---
+++ script/js.js
@@ -0,0 +1,34 @@
+function BMI(weight, height){
+  return weight/(height * height);
+}
+
+function getBMI(){
+  var weight = document.getElementById('weight').value;
+  var height = document.getElementById('height').value;
+  var showElement = document.getElementById('show');//show ra màn hình
+  if(weight == "" || height == ""){
+      showElement.innerHTML = "Vui lòng nhập đầy đủ dữ liệu";
+  }else if(isNaN(weight) || isNaN(height)){
+    showElement.innerHTML ="Dữ liệu nhập vào không phải số";
+  }else{
+    weight = parseFloat(weight);
+    height = parseFloat(height);
+    if(weight < 0 && height < 0){
+      showElement.innerHTML = "Dữ liệu nhập vào phải lớn hơn 0";
+    }else{
+      var bmi = BMI(weight,height);
+      var message = "Chỉ số BMI "+ bmi + " - " +"Người "+ phanLoai(bmi);
+      showElement.innerHTML = message;
+    }
+  }
+}
+
+function phanLoai(bmi){
+  if(bmi<18.5){
+    return "Thiếu cân";
+  }else if(bmi > 25){
+    return "Thừa cân";
+  }else{
+    return "Cân đối";
+  }
+}
